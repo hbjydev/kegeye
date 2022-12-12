7283d2d5afe47ad6a6216b3b3d07a33e2546a8cd
Hayden Young <hayden@hbjy.dev>
2022-12-12 21:48:35 +0000

    feat: implement dex endpoint

--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/go-git/go-git/v5 v5.5.1
 	github.com/google/go-github/v48 v48.2.0
 	github.com/rwxrob/bonzai v0.20.9
+	github.com/rwxrob/fn v0.4.0
+	github.com/rwxrob/keg v0.9.0
 	go.uber.org/zap v1.23.0
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -65,12 +67,10 @@ require (
 	github.com/rwxrob/compcmd v0.3.0 // indirect
 	github.com/rwxrob/compfile v0.1.12 // indirect
 	github.com/rwxrob/conf v0.8.2 // indirect
-	github.com/rwxrob/fn v0.4.0 // indirect
 	github.com/rwxrob/fs v0.20.0 // indirect
 	github.com/rwxrob/grep v0.2.5 // indirect
 	github.com/rwxrob/help v0.7.2 // indirect
 	github.com/rwxrob/json v0.8.0 // indirect
-	github.com/rwxrob/keg v0.9.0 // indirect
 	github.com/rwxrob/pegn v0.2.1 // indirect
 	github.com/rwxrob/structs v0.6.0 // indirect
 	github.com/rwxrob/term v0.2.9 // indirect
--- handlers/dex.go
+++ handlers/dex.go
@@ -2,15 +2,18 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/go-git/go-git/v5/storage/memory"
 	"github.com/google/go-github/v48/github"
 	"github.com/hbjydev/kegeye/keg"
+	_keg "github.com/rwxrob/keg"
 	"go.uber.org/zap"
 	"gopkg.in/yaml.v3"
 )
@@ -23,6 +26,7 @@ func RegisterDexHandler(e *gin.Engine, l *zap.Logger) {
 	r := e.Group("/keg/github")
 	{
 		r.GET("/:owner/:repo", githubRepoExists(l), fetchKeg)
+		r.GET("/:owner/:repo/dex", githubRepoExists(l), fetchDexEntries)
 	}
 }
 
@@ -34,78 +38,128 @@ func githubRepoExists(l *zap.Logger) gin.HandlerFunc {
 		client := github.NewClient(nil)
 		repo, _, err := client.Repositories.Get(context.TODO(), owner, name)
 		if err != nil {
-      if strings.HasSuffix(err.Error(), ": 404 Not Found []") {
-        c.AbortWithStatusJSON(404, gin.H{"error": "repository does not exist"})
-        return
-      }
+			if strings.HasSuffix(err.Error(), ": 404 Not Found []") {
+				c.AbortWithStatusJSON(404, gin.H{"error": "repository does not exist"})
+				return
+			}
 
 			_ = c.AbortWithError(500, err)
 			return
 		}
 
 		c.Set("cloneUrl", repo.GetCloneURL())
-    c.Set("branch", repo.GetDefaultBranch())
+		c.Set("branch", repo.GetDefaultBranch())
 	}
 }
 
 func fetchKeg(c *gin.Context) {
 	cloneUrl := c.GetString("cloneUrl")
-  branch := c.GetString("branch")
+	branch := c.GetString("branch")
 
-  // Clone the branch set by the middleware
-  s := memory.NewStorage()
-  r, err := git.Clone(s, nil, &git.CloneOptions{
-    URL: cloneUrl,
-    ReferenceName: plumbing.NewBranchReferenceName(branch),
-    Depth: 1,
-    SingleBranch: true,
-  })
+	keg, _, err := fetchKegInfo(cloneUrl, branch)
+	if err != nil {
+		_ = c.AbortWithError(500, err)
+		return
+	}
 
+	c.JSON(200, gin.H{"keg": keg})
+}
+
+func fetchDexEntries(c *gin.Context) {
+	cloneUrl := c.GetString("cloneUrl")
+	branch := c.GetString("branch")
+
+	keg, commit, err := fetchKegInfo(cloneUrl, branch)
+	if err != nil {
+		_ = c.AbortWithError(500, err)
+		return
+	}
+
+  dex, err := fetchDex(keg, commit)
   if err != nil {
-    _ = c.AbortWithError(500, err)
-    return
+		_ = c.AbortWithError(500, err)
+		return
   }
 
-  // Get the current state of <default branch>
-  head, err := r.Head()
-  if err != nil {
-    _ = c.AbortWithError(500, err)
-    return
-  }
+	c.JSON(200, dex.ByID())
+}
 
-  // Get the commit object for the latest commit to <default branch>
-  commit, err := r.CommitObject(head.Hash())
+func fetchDex(keg *keg.KegInfo, commit *object.Commit) (*_keg.Dex, error) {
+  file, err := getKegFile("dex/changes.md", commit)
   if err != nil {
-    _ = c.AbortWithError(500, err)
-    return
-  }
-
-  file, err := commit.File("keg")
-  if err != nil {
-    // If the keg is not at /, try /docs/
-    if err.Error() == "file not found" {
-      file, err = commit.File("docs/keg")
-      if err != nil {
-        _ = c.AbortWithError(500, err)
-        return
-      }
-    } else {
-      _ = c.AbortWithError(500, err)
-      return
-    }
+    return nil, err
   }
 
   contents, err := file.Contents()
   if err != nil {
-    _ = c.AbortWithError(500, err)
-    return
+    return nil, err
   }
 
-  var keg keg.KegInfo
-  if err := yaml.Unmarshal([]byte(contents), &keg); err != nil {
-    _ = c.AbortWithError(500, err)
-    return
+  dex, err := _keg.ParseDex(contents) 
+  if err != nil {
+    return nil, err
   }
 
-	c.JSON(200, gin.H{"keg": keg})
+  return dex, nil
+}
+
+func fetchKegInfo(cloneUrl string, branch string) (*keg.KegInfo, *object.Commit, error) {
+	// Clone the branch set by the middleware
+	s := memory.NewStorage()
+	r, err := git.Clone(s, nil, &git.CloneOptions{
+		URL:           cloneUrl,
+		ReferenceName: plumbing.NewBranchReferenceName(branch),
+		Depth:         1,
+		SingleBranch:  true,
+	})
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Get the current state of <default branch>
+	head, err := r.Head()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Get the commit object for the latest commit to <default branch>
+	commit, err := r.CommitObject(head.Hash())
+	if err != nil {
+		return nil, nil, err
+	}
+
+  file, err := getKegFile("keg", commit)
+  if err != nil {
+    return nil, nil, err
+  }
+
+	contents, err := file.Contents()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var keg keg.KegInfo
+	if err := yaml.Unmarshal([]byte(contents), &keg); err != nil {
+		return nil, nil, err
+	}
+
+	return &keg, commit, nil
+}
+
+func getKegFile(path string, commit *object.Commit) (*object.File, error) {
+	file, err := commit.File(path)
+	if err != nil {
+		// If the keg is not at /, try /docs/
+		if err.Error() == "file not found" {
+			file, err = commit.File(fmt.Sprintf("docs/%v", path))
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			return nil, err
+		}
+	}
+
+  return file, nil
 }
